a65197a9f919c4bdbdece80c30ca04c6d726a275
Tomas Williston <tomaswilliston@gmail.com>
2025-11-09 13:03:11 -0500

    Testing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,36 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    String url = "jdbc:postgresql://localhost:5432/Assignment 3";
+    String username = "postgres";
+    String password = "postgres";
+    public static void main(String[] args) {
+        new Main().getAllStudents();
+    }
+
+    public void getAllStudents() {
+
+        System.out.println("ID\tName\t\tEmail\t\t\t\t\tEnrollment");
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection conn = DriverManager.getConnection(url, username, password);
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from students");
+            while (rs.next()) {
+                System.out.print(rs.getString("student_id") + "\t");
+                System.out.print(rs.getString("first_name") + " ");
+                System.out.print(rs.getString("last_name") + "\t");
+                System.out.print(rs.getString("email") + "\t");
+                System.out.println(rs.getString("enrollment_date"));
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
